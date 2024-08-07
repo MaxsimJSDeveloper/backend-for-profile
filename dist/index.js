2c6974594aa7b662e3550ae66915b1a213377978
Holovko Maksym <golovkomaksim852@gmail.com>
2024-08-07 15:33:13 +0300

    add-yaml

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,14 +15,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const initMongoDB_1 = require("./db/initMongoDB");
-const profile_1 = __importDefault(require("./routes/profile"));
 const notFoundHendler_1 = require("./middlewares/notFoundHendler");
 const errorHendler_1 = require("./middlewares/errorHendler");
 const env_1 = require("./utils/env");
+const profile_1 = __importDefault(require("./routes/profile"));
 const app = (0, express_1.default)();
 const PORT = Number((0, env_1.env)("PORT", "3000"));
-const startServer = async () => {
-    await (0, initMongoDB_1.initMongoDB)();
+const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, initMongoDB_1.initMongoDB)();
     app.use(profile_1.default);
     app.use((0, cors_1.default)());
     app.listen(PORT, () => {
@@ -21,5 +30,5 @@ const startServer = async () => {
     });
     app.use("*", notFoundHendler_1.notFoundHandler);
     app.use(errorHendler_1.errorHandler);
-};
+});
 startServer();
